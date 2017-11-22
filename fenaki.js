15628b25439c0c235aaaaaa4ce6939e762d91d53
dkolovos <dkolovos@cs.york.ac.uk>
2017-11-22 02:01:17 +0000

    Fixed popup html

--- fenaki.js
+++ fenaki.js
@@ -51,10 +51,8 @@ chrome.storage.sync.get({
 
 document.querySelector('#go-to-options').addEventListener("click", function() {
   if (chrome.runtime.openOptionsPage) {
-    // New way to open options pages, if supported (Chrome 42+).
     chrome.runtime.openOptionsPage();
   } else {
-    // Reasonable fallback.
     window.open(chrome.runtime.getURL('options.html'));
   }
 });
